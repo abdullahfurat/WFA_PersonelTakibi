290a8d398b4ecf599ef285b9ad8de766078b84d7
muratvuranok <mrtvrnk@gmail.com>
2020-01-04 14:35:35 +0300

    Add project files.

---
+++ WFAPersonelTakibi/Form1.cs
@@ -0,0 +1,24 @@
+﻿using MetroFramework.Forms;
+using System;
+
+namespace WFAPersonelTakibi
+{
+    public partial class Form1 : MetroForm
+    {
+        private IService<Employee> _employeeService;
+        public Form1()
+        {
+            InitializeComponent(); 
+            _employeeService = new EmployeeService();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = txtFirstName.Text;
+
+            _employeeService.Add(employee);
+             
+        }
+    }
+}
---
+++ WFAPersonelTakibi/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace WFAPersonelTakibi
+{
+    public partial class Form2 : MetroForm
+    {
+        private IService<Employee> _employeeService;
+        private IService<Department> _departmentService;
+        public Form2()
+        {
+            InitializeComponent();
+            _employeeService = new EmployeeService();
+        }
+
+        void PersonelListesi()
+        {
+            dgvEmployees.DataSource = _employeeService.GetAll();
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
